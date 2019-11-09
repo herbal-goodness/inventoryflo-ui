6aa3f26785c4da4723d9bf730b366ab43ffa36ea
robsli <robbieli121.zy@gmail.com>
2019-11-08 22:41:31 -0500

    Update Header with responsive styles

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,41 +1,77 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import Button from 'react-bootstrap/Button';
 import { LinkContainer } from 'react-router-bootstrap';
+import styled from 'styled-components';
+
+const StyledButtonToolbar = styled(ButtonToolbar)`
+	display: flex;
+	flex-wrap: wrap;
+	padding: 16px;
+
+	@media (min-width: 992px) {
+		flex-wrap: nowrap;
+		padding: 0 16px;
+		width: 100%;
+	}
+`;
+
+const StyledLink = styled(LinkContainer)`
+	width: 100%;
+
+	:not(:last-of-type) {
+		margin-bottom: 16px;
+	}
+
+	@media screen and (min-width: 992px) {
+		min-width: 100px;
+		margin-bottom: 0 !important;
+		margin-left: 16px;
+		width: unset;
+
+		:first-of-type {
+			margin-left: 32px;
+		}
+	}
+`;
+
+const StyledNav = styled(Nav)`
+	width: 100%;
+`;
 
 const Header = () => {
 
 	return (
 		<Navbar bg="dark" expand="lg">
 			<LinkContainer to="/">
-				<Navbar.Brand>Inventory Flo</Navbar.Brand>
+				<Navbar.Brand><img src="/header-icon.png" alt="Inventory Flo Home" /></Navbar.Brand>
 			</LinkContainer>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
-					<ButtonToolbar>
-						<LinkContainer exact to="/items" isActive={(match, location) => location.pathname.includes('/items')}>
+				<StyledNav>
+					<StyledButtonToolbar>
+						<StyledLink exact to="/items" isActive={(match, location) => location.pathname.includes('/items')}>
 							<Button>Items</Button>
-						</LinkContainer>
-						<LinkContainer exact to="/inventory-summary" isActive={(match, location) => location.pathname.includes('/inventory-summary')}>
+						</StyledLink>
+						<StyledLink exact to="/inventory-summary" isActive={(match, location) => location.pathname.includes('/inventory-summary')}>
 							<Button>Inventory Summary</Button>
-						</LinkContainer>
-						<LinkContainer exact to="/contacts" isActive={(match, location) => location.pathname.includes('/contacts')}>
+						</StyledLink>
+						<StyledLink exact to="/contacts" isActive={(match, location) => location.pathname.includes('/contacts')}>
 							<Button>Contacts</Button>
-						</LinkContainer>
-						<LinkContainer exact to="/warehouses" isActive={(match, location) => location.pathname.includes('/warehouses')}>
+						</StyledLink>
+						<StyledLink exact to="/warehouses" isActive={(match, location) => location.pathname.includes('/warehouses')}>
 							<Button>Warehouses</Button>
-						</LinkContainer>
-						<LinkContainer exact to="/purchase-orders" isActive={(match, location) => location.pathname.includes('/purchase-orders')}>
+						</StyledLink>
+						<StyledLink exact to="/purchase-orders" isActive={(match, location) => location.pathname.includes('/purchase-orders')}>
 							<Button>Purchase Orders</Button>
-						</LinkContainer>
-						<LinkContainer exact to="/transfers" isActive={(match, location) => location.pathname.includes('/transfers')}>
+						</StyledLink>
+						<StyledLink exact to="/transfers" isActive={(match, location) => location.pathname.includes('/transfers')}>
 							<Button>Transfers</Button>
-						</LinkContainer>
-					</ButtonToolbar>
-				</Nav>
+						</StyledLink>
+					</StyledButtonToolbar>
+				</StyledNav>
 			</Navbar.Collapse>
 		</Navbar>
 	);
