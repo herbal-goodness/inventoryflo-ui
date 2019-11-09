4a478c545b7afb72bfcab21e0d52ecf862c4d94c
robsli <robbieli121.zy@gmail.com>
2019-11-09 01:30:44 -0500

    Update eslint rules and package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './assets/iconic/font/css/open-iconic-bootstrap.min.css';
-import './assets/index.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import App from './components/App';
 import InventoryFlo from './components/InventoryFlo';
 
 render(
