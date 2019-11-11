f10bfb2da622ebd47631fb72d3446fb13d8bb0b9
robsli <robbieli121.zy@gmail.com>
2019-11-11 15:45:27 -0500

    Update Warehouses page

--- src/components/warehouses/WarehousesSummary.js
+++ src/components/warehouses/WarehousesSummary.js
@@ -1,59 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function WarehouseSummary() {
+import mockWarehouses from '../../mocks/Warehouses';
+import { getWarehouses } from '../../services/warehouses.service';
+import { PageHeader } from '../shared/StyledComponents';
+import WarehouseCard from './WarehouseCard';
+
+const Wrapper = styled.section`
+
+	width: 100%;
+`;
+
+const WarehousesSummary = () => {
+	const [warehouses, setWarehouses] = useState([]);
+
+	useEffect(() => {
+		getWarehouses().then(
+			({ data }) => {
+				setWarehouses(data);
+			},
+			(error) => {
+				console.error(error);
+
+				setWarehouses(mockWarehouses);
+			}
+		);
+	}, []);
+
 	return (
-		<>
-			<Link to="/warehouses/new" className="btn orange-back mx-2 my-2">
-				New Warehouse
-			</Link>
-			<br />
-			<CardRow
-				title="E Fulfillment Warehouse"
-				type="EFS"
-				addr="2150 South Central Express"
-				city="McKinney"
-				state="Texas"
-				country="USA"
-				email="abc@email.com"
-			/>
-			<CardRow
-				title="Davidsons Warehouse"
-				type="EFS"
-				addr="2150 South Central Express"
-				city="McKinney"
-				state="Texas"
-				country="USA"
-				email="abc@email.com"
-			/>
-		</>
+		<Wrapper>
+			<PageHeader>Warehouses Summary</PageHeader>
+			{ warehouses && (
+				warehouses.map(warehouse => (
+					<WarehouseCard
+						address={warehouse.address}
+						email={warehouse.email}
+						key={`warehouse-${warehouse.name}`}
+						name={warehouse.name}
+						phone={warehouse.phone}
+					/>
+				))
+			)}
+		</Wrapper>
 	);
-}
+};
 
-function CardRow({ title, type, addr, city, state, country, email }) {
-	return (
-		<div className="row justify-content-md-center">
-			<div className="card col-md-3 text-center">
-				<div className="card-body">
-					<h1 className="card-title">{title}</h1>
-					<h1 className="card-text">({type})</h1>
-				</div>
-			</div>
-			<div className="card col-md-6">
-				<div className="card-body">
-					{addr}
-					<br />
-					{city}
-					<br />
-					{state}
-					<br />
-					{country}
-					<br />
-					{email}
-				</div>
-			</div>
-		</div>
-	);
-}
-
-export default WarehouseSummary;
+export default WarehousesSummary;
